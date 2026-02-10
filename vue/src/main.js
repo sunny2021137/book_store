49f560541a73a7098373ea2a43cc5dc3fd88513a
sunny2021137 <sunny2021137@gmail.com>
2026-02-10 21:32:05 +0800

    vue ok

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '@/assets/global.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
+
+// 註冊 Element Plus 的圖示
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+app.component(key, component)
+}
 
 app.mount('#app')
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,7 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path: '/', redirect: '/manager/home'},
+    {path: '/manager', component: () => import('../views/Mananger.vue'), children: [
+      {path: 'home', meta:{title: '主頁面'}, component: () => import('../views/Home.vue')},
+      {path: 'test', meta:{title: '測試頁面'}, component: () => import('../views/Test.vue')},
+      {path: 'data', meta:{title: '數據展示頁面'}, component: () => import('../views/Data.vue')},
+    ]},
+    {path: '/404', name: 'NotFound', component: () => import('../views/404.vue')},
+    {path: '/:pathMatch(.*)', redirect: '/404' }
+    
+  ],
 })
+
+router.beforeEach((to, from, next) => {
+  // 在路由切換前執行的邏輯
+  document.title = to.meta.title || 'Default Title'; // 設置頁面標題
+  console.log('正在導航到:', to.path);
+  next(); // 繼續導航
+});
 
 export default router
